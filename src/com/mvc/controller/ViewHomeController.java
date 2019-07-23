d5b91fd73a87c33f8fb4e69e1d126db01dec4fe3
mahammed88 <sbali623@gmail.com>
2019-07-23 21:50:15 +0530

    jsp change

---
+++ src/com/mvc/controller/ViewHomeController.java
@@ -0,0 +1,19 @@
+package com.mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class ViewHomeController  extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		
+		return new ModelAndView("home");
+	}
+
+	
+}
